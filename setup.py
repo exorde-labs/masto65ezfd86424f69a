54f089e03f06b51330dea65fc9d45a2041ae616f
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-08-08 12:29:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="masto65ezfd86424f69a",
-    version="0.0.7",
+    version="0.0.8",
     packages=find_packages(),
     install_requires=[
         "exorde_data",
